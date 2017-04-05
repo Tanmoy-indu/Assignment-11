d16fc91baff208668a452fd87af6a79466d43e57
Tanmoy Indu <tanmoy433@gmail.com>
2017-04-05 15:58:00 +0000

    Assignment-11

---
+++ helloworld.js
@@ -0,0 +1,27 @@
+ console.log("Hello World")
+ console.log(process.argv)
+  var fs = require('fs') 
+   function listener(socket) { /* ... */ }
+   var net = require('net')  
+     var server = net.createServer(function (socket) {  
+       // socket handling logic  
+     })  
+     server.listen(8000)
+     date.getFullYear()  
+     date.getMonth()     // starts at 0  
+     date.getDate()      // returns the day of month  
+     date.getHours()  
+     date.getMinutes()
+     
+     function callback (request, response) { /* ... */ } 
+     var http = require('http')  
+     var server = http.createServer(function (req, res) {  
+       // request handling logic...  
+     })  
+     server.listen(8000)
+     
+     var map = require('through2-map')  
+     inStream.pipe(map(function (chunk) {  
+       return chunk.toString().split('').reverse().join('')  
+     })).pipe(outStream) 
+    
